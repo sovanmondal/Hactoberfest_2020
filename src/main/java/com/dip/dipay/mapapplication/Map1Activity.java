019d50ffe2ec602b718a601a2a5333da341eeb12
Dipayan Manna <54394778+dipayan000@users.noreply.github.com>
2020-10-14 15:40:02 +0530

    Map Application
    it's an Map application

---
+++ src/main/java/com/dip/dipay/mapapplication/Map1Activity.java
@@ -0,0 +1,13 @@
+package com.dip.dipay.mapapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Map1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map1);
+    }
+}
